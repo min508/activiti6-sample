3c238991a300ca7f42102c89f1a095ae2eb6d161
min508 <405409990@qq.com>
2019-10-28 00:21:59 +0800

    springboot2.0与activiti6.0集成

--- springboot/src/main/java/com/imooc/activiti/SpringbootApplication.java
+++ springboot/src/main/java/com/imooc/activiti/SpringbootApplication.java
@@ -1,9 +1,10 @@
 package com.imooc.activiti;
 
+import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SpringbootApplication {
 
     public static void main(String[] args) {
--- springboot/src/test/java/com/imooc/activiti/SpringbootApplicationTests.java
+++ springboot/src/test/java/com/imooc/activiti/SpringbootApplicationTests.java
@@ -1,13 +1,31 @@
 package com.imooc.activiti;
 
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.runtime.ProcessInstance;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringbootApplicationTests {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringbootApplicationTests.class);
+
+    @Autowired
+    private RuntimeService runtimeService;
+
     @Test
     void contextLoads() {
+
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("my-process");
+
+        LOGGER.info("processInstance = {}",processInstance);
+
     }
 
 }
